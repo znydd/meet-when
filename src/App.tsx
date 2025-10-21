e2b6bda5460180d90daec0ebe3f630866a94f6eb
Znyd <mailboxofnabil@gmail.com>
2025-10-21 22:54:08 +0600

    testing insert UserName UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import PWABadge from "./PWABadge.tsx";
 import Instruction from "./components/instruction.tsx";
 import PasteAsHtml from "./components/pasteAsHtml.tsx";
+import InsertUserName from "./components/insertUserName.tsx";
 
 function App() {
   return (
     <>
       <div className="h-screen flex flex-col items-center bg-[#fafafa]">
+        {/*<InsertUserName />*/}
         <Instruction />
         <PasteAsHtml />
         <PWABadge />
