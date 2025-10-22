5a37e419a3cbe8567273c29f7532ff73ace17bb8
Znyd <mailboxofnabil@gmail.com>
2025-10-23 03:02:40 +0600

    Initial input wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import PWABadge from "./PWABadge.tsx";
-import Instruction from "./components/instruction.tsx";
-import PasteAsHtml from "./components/pasteAsHtml.tsx";
-import InsertUserName from "./components/insertUserName.tsx";
-
+import InitialUserInput from "./components/initialUserInput.tsx";
 function App() {
   return (
     <>
-      <div className="h-screen flex flex-col items-center bg-[#fafafa]">
-        {/*<InsertUserName />*/}
-        <Instruction />
-        <PasteAsHtml />
+      <div>
+        <InitialUserInput />
         <PWABadge />
       </div>
     </>
