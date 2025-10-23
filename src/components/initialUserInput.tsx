b9750d5eb69f1fd2c314605a64caa536bf71cf3a
Znyd <mailboxofnabil@gmail.com>
2025-10-24 01:56:27 +0600

    save initial user to admin table

--- src/components/initialUserInput.tsx
+++ src/components/initialUserInput.tsx
@@ -2,12 +2,17 @@ import Instruction from "./instruction";
 import PasteAsHtml from "./pasteAsHtml";
 import InsertNameId from "./insertNameId";
 import type { User, EachDay, Status } from "../types/type";
+import { createAdmin } from "../lib/dbQuery";
 import { useState } from "react";
 
-function InitialUserInput() {
-  const [user, setUser] = useState<User>();
+function InitialUserInput({
+  onInputDone,
+}: {
+  onInputDone: (inputDone: boolean) => void;
+}) {
   const [routine, setRoutine] = useState<EachDay | null>(null);
   const [parsingStatus, setParsingStatus] = useState<Status>("idle");
+  const [statusModal, setStatusModal] = useState<"show" | "hide">("hide");
   const handleRoutine = (
     parsedStatus: Status,
     parsedRoutine: EachDay | null,
@@ -19,7 +24,7 @@ function InitialUserInput() {
     console.log(routine);
   };
 
-  const handleNameId = (name: string, id: string) => {
+  const handleNameId = async (name: string, id: string) => {
     console.log(name, id);
     if (routine && name && id) {
       const newUser: User = {
@@ -27,13 +32,28 @@ function InitialUserInput() {
         studentId: id,
         routine: routine,
       };
-      setUser(newUser);
+      const studentId: string = await createAdmin(newUser);
+      if (studentId) {
+        setStatusModal("show");
+        setTimeout(() => {
+          setStatusModal("hide");
+          onInputDone(true);
+        }, 1000);
+      }
       console.log(newUser);
     }
   };
 
   return (
     <>
+      {statusModal === "show" && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-transparent bg-opacity-50 backdrop-blur-sm text-black">
+          <div className="text-center border showdow-sm bg-white px-14 py-10 rounded-lg">
+            Done
+          </div>
+        </div>
+      )}
+
       {parsingStatus === "success" ? (
         <div className="h-screen flex flex-col items-center bg-[#fafafa]">
           <InsertNameId onNameId={handleNameId} />
